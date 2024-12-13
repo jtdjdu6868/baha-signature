7669118e2d6baf38d4f694988e370bb9f5e6aa14
jtdjdu6868 <99466048+jtdjdu6868@users.noreply.github.com>
2024-12-14 01:32:18 +0800

    support .png and .svg path

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get("/", (req, res) => {
 
 const weather_sign_route = require("./weather_sign/handle.js");
 
-app.use("/sign/weather_sign.png", weather_sign_route);
+app.use(["/sign/weather_sign.png", "/sign/weather_sign.svg"], weather_sign_route);
 
 const ohboy3am_sign_route = require("./ohboy3am_sign/handle.js");
 
-app.use("/sign/ohboy3am_sign.png", ohboy3am_sign_route);
+app.use(["/sign/ohboy3am_sign.png", "/sign/ohboy3am_sign.svg"], ohboy3am_sign_route);
 
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
